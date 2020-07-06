3cb0e10840a32ae351bf6f1ab5ff7da123b3a0bb
Greg <werner.greg@gmail.com>
2020-07-06 16:37:20 -0400

    update release
    Signed-off-by: Greg <werner.greg@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="illumidesk-theia-proxy",
-    version='0.1.0',
+    version='0.1.1',
     url="https://github.com/illumidesk/illumidesk-theia-proxy",
     author="IllumiDesk Team",
     description="hello@illumidesk.com",
