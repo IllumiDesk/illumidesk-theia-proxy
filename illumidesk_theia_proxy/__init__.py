78175faca63d5e4ad867032604635837edf81cfc
Greg <werner.greg@gmail.com>
2020-07-06 16:06:45 -0400

    udpate cmd
    Signed-off-by: Greg <werner.greg@gmail.com>

--- illumidesk_theia_proxy/__init__.py
+++ illumidesk_theia_proxy/__init__.py
@@ -10,10 +10,17 @@ import shutil
 def setup_theia():
     # Make sure theia is in $PATH
     def _theia_command(port):
-        full_path = shutil.which('theia')
+        executable = 'theia'
+        full_path = shutil.which(executable)
+
+        # Start theia in NODE_LIB_PATH env variable if set
+        # If not, start in 'current directory', which is $REPO_DIR in mybinder
+        # but /home/jovyan (or equivalent) in JupyterHubs
+        working_dir = os.getenv('NODE_LIB_PATH', '.')
+
         if not full_path:
             raise FileNotFoundError('Can not find theia executable in $PATH')
-        return ['theia', 'start', '$NODE_LIB_PATH', '--hostname=0.0.0.0', '--port=' + str(port)]
+        return ['theia', 'start', working_dir, '--hostname=0.0.0.0', '--port=' + str(port)]
     return {
         'command': _theia_command,
         'environment': {
